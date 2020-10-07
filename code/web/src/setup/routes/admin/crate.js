62d4f3356654f0490358fafd9258c360834301da
Jordy Shryock <jordy1611@gmail.com>
2020-10-07 12:23:21 -0600

    Add console to crate onClick

--- code/web/src/setup/routes/admin/crate.js
+++ code/web/src/setup/routes/admin/crate.js
@@ -1,26 +1,26 @@
 // App Imports
-import params from '../../../setup/config/params'
-import CrateList from '../../../modules/admin/crate/List'
-import CrateCreateOrEdit from '../../../modules/admin/crate/CreateOrEdit'
+import params from "../../../setup/config/params";
+import CrateList from "../../../modules/admin/crate/List";
+import CrateCreateOrEdit from "../../../modules/admin/crate/CreateOrEdit";
 
 // Admin crate routes
 export const crateList = {
-  path: '/admin/crates',
+  path: "/admin/crates",
   component: CrateList,
   auth: true,
-  role: params.user.roles.admin
-}
+  role: params.user.roles.admin,
+};
 
 export const crateCreate = {
-  path: '/admin/crate/create',
+  path: "/admin/crate/create",
   component: CrateCreateOrEdit,
   auth: true,
-  role: params.user.roles.admin
-}
+  role: params.user.roles.admin,
+};
 
 export const crateEdit = {
-  path: (id = ':id') => (`/admin/crate/${ id }/edit`),
+  path: (id = ":id") => `/admin/crate/${id}/edit`,
   component: CrateCreateOrEdit,
   auth: true,
-  role: params.user.roles.admin
-}
+  role: params.user.roles.admin,
+};
