f30420ec5ab368a905111fdeeb4b96750bc186eb
Jordy Shryock <jordy1611@gmail.com>
2020-10-08 17:02:59 -0600

    Annotate user paths file

--- code/web/src/setup/routes/user.js
+++ code/web/src/setup/routes/user.js
@@ -1,30 +1,33 @@
 // App Imports
-import Login from '../../modules/user/Login'
-import Signup from '../../modules/user/Signup'
-import Profile from '../../modules/user/Profile'
-import Subscriptions from '../../modules/user/Subscriptions'
+import Login from "../../modules/user/Login";
+import Signup from "../../modules/user/Signup";
+import Profile from "../../modules/user/Profile";
+import Subscriptions from "../../modules/user/Subscriptions";
 
 // User routes
 export default {
   login: {
-    path: '/user/login',
-    component: Login
+    path: "/user/login",
+    component: Login,
   },
 
   signup: {
-    path: '/user/signup',
-    component: Signup
+    path: "/user/signup",
+    component: Signup,
   },
 
   profile: {
-    path: '/user/profile',
+    path: "/user/profile",
     component: Profile,
-    auth: true
+    auth: true,
   },
 
   subscriptions: {
-    path: '/user/subscriptions',
+    path: "/user/subscriptions",
     component: Subscriptions,
-    auth: true
-  }
-}
+    auth: true,
+  },
+};
+
+// All user related paths. I think we're going to need to create a path here for /style-preferences and the survey
+// Probably a survey object with path /user/style-preferences & component Survey & auth: true
