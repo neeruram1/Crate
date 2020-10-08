0a94f0aa8be1464761d9c697c8768ef42d26bbbd
“Neeru <neeram85@gmail.com>
2020-10-07 19:34:55 -0600

    Annotate modules/subscription/model.js

--- code/api/src/modules/subscription/model.js
+++ code/api/src/modules/subscription/model.js
@@ -18,3 +18,6 @@ module.exports = function(sequelize, DataTypes) {
 
   return Subscription
 }
+
+//Annotation:
+//This file is defining our subscription model. The subscription model has attributes of userID and crateID. It has belongs to relationships with the user model and the crate model. It is the joins table between users and crates. 
