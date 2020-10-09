ab25d7907ad1cd85f5aa58e41cd15db4dde7f442
“Neeru <neeram85@gmail.com>
2020-10-08 19:06:01 -0600

    Annotated  code/api/src/modules/subscription/query.js

--- code/api/src/modules/subscription/query.js
+++ code/api/src/modules/subscription/query.js
@@ -25,3 +25,6 @@ export const subscription = {
   },
   resolve: get
 }
+
+//Annotation:
+//This file is defining Graph QL queries for finding all products and finding a single product by id. It's importing the getAll and getById functions from the resolver.js file and invoking those functions in the appropriate query. For example - in the productById query - the resolve: getById is handling the query for the fields contained in the query, and it is generating the appropriate response.
