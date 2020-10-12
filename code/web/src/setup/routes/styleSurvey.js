d6b26b9a7825eacebdd081ce1356a1575eebe2ec
Tyler Haglund <relyt4me@gmail.com>
2020-10-12 15:30:36 -0600

    Create Style survey route

---
+++ code/web/src/setup/routes/styleSurvey.js
@@ -0,0 +1,11 @@
+// App Imports
+import List from '../../modules/styleSurvey/List'
+
+// styleSurvey routes
+export default {
+  list: {
+    path: '/style-preferences',
+    component: List,
+    auth: true
+  }
+}
