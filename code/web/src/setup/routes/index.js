69d4d77badd19ef0750665fc1c70dca13ef385ed
Tyler Haglund <relyt4me@gmail.com>
2020-10-12 15:30:23 -0600

    Add the new page to the routes index

--- code/web/src/setup/routes/index.js
+++ code/web/src/setup/routes/index.js
@@ -5,9 +5,10 @@ import home from './home'
 import user from './user'
 import product from './product'
 import crate from './crate'
+import styleSurvey from './styleSurvey'
 
 // Combined routes
-export const routes = Object.assign(admin, home, user, product, crate)
+export const routes = Object.assign(admin, home, user, product, crate, styleSurvey)
 
 // API Routes
 export const routeApi = APP_URL_API
