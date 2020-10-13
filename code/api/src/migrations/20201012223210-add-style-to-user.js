df720eaadf018daedeb1a5e3b4c6b85af5713a4e
“Neeru <neeram85@gmail.com>
2020-10-13 16:24:54 -0600

    Added migration to give a user a styleId

--- code/api/src/migrations/20201012223210-add-style-to-user.js
+++ code/api/src/migrations/20201012223210-add-style-to-user.js
@@ -1,39 +1,18 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-     return queryInterface.addColumn( 'Users', 'styleId', Sequelize.INTEGER,
-      references: {
-        model: 'styles',
-        key: 'id'
-      },
-      allowNull: true,
-      defaultValue: null
-      });
+    return Promise.all([
+      queryInterface.addColumn('users', 'styleId', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'styles',
+          key: 'id'
+        },
+        allowNull: true,
+      }),
+    ]);
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-    return queryInterface.removeColumn( 'Users', 'styleId',
-      references: {
-       model: 'styles',
-       key: 'id'
-     },
-     allowNull: true,
-     defaultValue: null
-   );
-  }
-}
+  down: (queryInterface) => {
+    return Promise.all([queryInterface.removeColumn('users', 'styleId')]);
+  },
+};
