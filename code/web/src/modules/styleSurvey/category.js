eeed52de44b02e41281459fe08596ee94c4addfb
Sean Leas <phoenix_24leas.com>
2020-10-13 17:33:17 -0600

    Setup Category component #22

---
+++ code/web/src/modules/styleSurvey/category.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Grid, GridCell } from '../../ui/grid'
+import { H3 } from '../../ui/typography'
+
+
+class Survey extends Component {
+
+  // // Runs on server only for SSR
+  // static fetchData({ store }) {
+  //   return store.dispatch(getCratesList('ASC'))
+  // }
+
+  // // Runs on client only
+  // componentDidMount() {
+  //   this.props.getCratesList('ASC')
+  // }
+
+  render() {
+    return (
+      <div>
+        <Grid style={{ backgroundColor: grey }}>
+          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+            <H3 font="secondary">Welcome to the Style Survey</H3>
+
+            <p style={{ marginTop: '1em', color: grey2 }}>Before we get you subscribed, let's get down your style.<br></br> In each of the following categories, choose the image that best describes you!</p>
+          </GridCell>
+        </Grid>
+        <Grid style={{ backgroundColor: secondary }}>
+          <GridCell
+            gutter={true}
+            style={{
+              minWidth: '80vw'
+            }}
+          >
+            <H4>YOLO</H4>
+          </GridCell><GridCell
+            gutter={true}
+            style={{
+              minWidth: '80vw'
+            }}
+          >
+            <H4>YOLO</H4>
+          </GridCell><GridCell
+            gutter={true}
+            style={{
+              minWidth: '80vw'
+            }}
+          >
+            <H4>YOLO</H4>
+          </GridCell>
+
+
+        </Grid>
+      </div>
+    )
+  }
+}
+
+// Component Properties
+// List.propTypes = {
+//   crates: PropTypes.object.isRequired,
+//   getCratesList: PropTypes.func.isRequired
+// }
+
+// Component State
+function listState(state) {
+  return {
+    crates: state.crates
+  }
+}
+
+export default connect(listState, null)(Category)
